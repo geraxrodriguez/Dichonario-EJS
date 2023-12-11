3943273cf607691af447994ce30ba2d3c910266b
Gerardo Rodriguez <gerarodriguez@proton.me>
2023-12-11 08:02:57 -0600

    server is up and running, connected ejs successfully

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const express = require('express')
+const app = express()
+const PORT = 2121
+
+app.set('view engine','ejs')
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
