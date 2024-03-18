42d123cbcad1ac857938abf636382cd3d5c7d38e
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-03-18 00:54:54 -0500

    added option to toggle between spanish message and english message, centered content

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+const divEN = document.getElementById('en')
+const divES = document.getElementById('es')
+
+function showEN() {
+    divEN.style.display = 'block'
+    divES.style.display = 'none'        
+}
+function showES() {
+    divES.style.display = 'block'
+    divEN.style.display = 'none'        
+}
