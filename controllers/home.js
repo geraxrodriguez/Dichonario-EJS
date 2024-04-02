2b7dd61867c48bc3d3819051784f1642fa0b691e
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-04-02 00:13:09 -0500

    got express working

--- controllers/home.js
+++ controllers/home.js
@@ -6,7 +6,7 @@ module.exports = {
         res.render('index.ejs')
     },
     getFormulario: (req, res) => {
-        res.render('agrega.ejs')
+        res.render('agrega.ejs', { messages: req.flash() })
     },
     getDichos: async (req, res) => {
         try {
@@ -19,15 +19,27 @@ module.exports = {
             res.status(500).send('Internal Server Error');
         }
     },
+
     agregaDicho: async (req, res) => {
         try {
             console.log('We reached the agrega controlla')
             
             //if user doesn't provide a dicho, significado, or translation
             //then we want to flash em
+            const { dicho, significado, translation } = req.body
+
+            if (!dicho || !significado || !translation) {
+                req.flash('message', 'Oops! Something is missing :( Please make sure you have filled out all fields')
+                res.redirect('/agrega')
+            }
+            else {
+                req.flash('message', 'Success! Your submission will be reviewed by a team member :)')
+                res.redirect('/agrega')
+            }
+            
 
             // await Dicho.create({ dicho: req.body.dicho, literal: req.body.literal, actual: req.body.actual })
-            res.redirect('/')
+            
         } catch (err) {
             console.error(err)
         }
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const session = require('express-session') 
 const flash = require('express-flash')
-const app = express()
+const session = require('express-session') 
+const express = require('express')
 const mongoose = require('mongoose')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const cors = require('cors')
+const app = express()
 
 require('dotenv').config({path: './config/.env'})
 
@@ -13,9 +13,23 @@ require('dotenv').config({path: './config/.env'})
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
+
+// guy in video require body-parser to do below, doesn't
+// look like i need to
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
 app.use(cors())
+
+//don't know what these do
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  }))
+  
+app.use(flash());
 
 // routes
 app.use('/', mainRoutes)
