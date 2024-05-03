ec246e1a3ec3ef5509b3ec251cb298ef94bc66ab
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-05-03 10:32:46 -0500

    findOneAndUpdate method used to update dichos is working successfully

--- controllers/admin.js
+++ controllers/admin.js
@@ -52,17 +52,67 @@ module.exports = {
         console.error(err)
     }
   },
-  
+
   // GET ALL DICHOS
   getDichos: async (req, res) => {
     try {
-        console.log('getting dichos...')
         const dichos = await Dicho.find() // find method w/out args returns all documents in collection
-        console.log(dichos.length)
         res.render('admin/dichos.ejs', {dichos: dichos})
     } catch (err) {
         console.error('Error fetching dichos:', err);
         res.status(500).send('Internal Server Error');
     }
   },
-}
+
+  // GET SINGLE DICHO
+  getOneDicho: async (req, res) => {
+    try {
+        const dicho = await Dicho.findById(req.params.id) // find method w/out args returns all documents in collection
+        res.render('admin/dicho.ejs', { dicho, })
+    } catch (err) {
+        console.error('Error fetching dichos:', err);
+        res.status(500).send('Internal Server Error');
+    }
+  },
+
+  // GET EDIT-DICHO PAGE
+  getEditDichoPage: async (req, res) => {
+    try {
+      const id = req.params.id
+      const dicho = await Dicho.findById(id);
+      res.render("admin/edit-dicho.ejs", { 
+        dicho,
+        id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  
+  // POST METHOD FOR EDITING DICHO
+  editDicho: async (req, res) => {    
+    try {
+
+      const id = req.params.id
+      const { dicho, meaning, example, variations, comments } = req.body
+      if (!dicho || !meaning || !example) {
+        req.flash('message', 'Oops! Something is missing :( Please make sure you have filled out all fields')
+        res.redirect(`/admin/edit-dicho/${id}`)
+      } else {
+        await Dicho.findByIdAndUpdate(id, {
+          dicho,
+          meaning,
+          example,
+          variations,
+          comments,
+        });
+        req.flash('message', 'Edit Accepted'); // route back to same dicho and show a success message
+        res.redirect(`/admin/dichos/${id}`);
+      };
+      // end try block
+    } catch (err) {
+      console.error('Error fetching dichos:', err);
+      res.status(500).send('Internal Server Error');
+    }; // end catch block
+  },
+};
