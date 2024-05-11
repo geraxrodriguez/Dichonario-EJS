10682509846bb0469cc842eaa8266d639f43d149
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-05-11 12:28:43 -0500

    test

--- server.js
+++ server.js
@@ -36,9 +36,6 @@ app.use(flash());
 app.use('/', mainRoutes)
 app.use('/admin', adminRoutes)
 
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server running on port ${process.env.PORT}`)
-// })
 
 //Connect to the database before listening
 connectDB().then(() => {
